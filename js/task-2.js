c6b814cd28191eb06f3cea7e368fd3a20af80ee8
Maf1ck <154552115+Maf1ck@users.noreply.github.com>
2025-02-01 10:37:12 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,30 +1,39 @@
-class Storage{
-  constructor(items) {
-    this.items = items;
-  }
+class Storage {
   #items;
-  getItems(){
+
+  constructor(items) {
+    this.#items = [...items]; 
+  }
+
+  getItems() {
     return this.#items;
   }
+
   addItem(newItem) {
     this.#items.push(newItem);
   }
+
   removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1); 
+    }
   }
 }
 
 
-
-
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+console.log("Початкові елементи:", storage.getItems()); 
+// ["Nanitoids", "Prolonger", "Antigravitator"]
 
 storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+console.log("Після додавання Droid:", storage.getItems()); 
+// ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
 storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+console.log("Після видалення Prolonger:", storage.getItems()); 
+// ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem("Scanner");
+console.log("Спроба видалити відсутній Scanner:", storage.getItems()); 
+// ["Nanitoids", "Antigravitator", "Droid"]
